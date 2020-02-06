fc88a116ffe62313df52b96ad4728eeb44b0b298
roey <roeybh@gmail.com>
2020-02-07 00:52:44 +0200

    add more articles. added stubs for feature selection. implemented mean acceleration.

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ class ActivityRecognition:
         self.__log_enabled = False                        
         self.__ground_truth_file_list = []
 
+        self.__all_imu_myo_fork_data = []
+        self.__imu_myo_fork_data_userid = []
+        self.__all_imu_myo_spoon_data = []
+        self.__imu_myo_spoon_data_userid = []
+        self.__imu_fork_eating = []
+        self.__imu_fork_non_eating = []
+        self.__imu_spoon_eating = []
+        self.__imu_spoon_non_eating = []
+
         print(os.path.realpath(__file__))
         self.__data_path = os.path.dirname(os.path.realpath(__file__))
         self.__number_of_users = 0
@@ -48,18 +57,7 @@ class ActivityRecognition:
         print ("Cleanup")
 
 
-    def data_setup(self):
-
-        # all should be class members
-        # should be defined in the constructor
-        all_imu_myo_fork_data = []
-        imu_myo_fork_data_userid = []
-        all_imu_myo_spoon_data = []
-        imu_myo_spoon_data_userid = []
-        imu_fork_eating = []
-        imu_fork_non_eating = []
-        imu_spoon_eating = []
-        imu_spoon_non_eating = []
+    def preprocessing(self):
         
         out_dir = 'out'
         fork = 'fork'
@@ -79,18 +77,18 @@ class ActivityRecognition:
                     # use regex to get the user id of fork IMU data
                     result = re.search('MyoData/(.*)/fork', file)
                     # save the user id
-                    imu_myo_fork_data_userid.append(result.group(1))
+                    self.__imu_myo_fork_data_userid.append(result.group(1))
                     # read the IMU data
                     imu_myo_fork_data=np.genfromtxt(file, dtype=float, delimiter=',',usecols=(1,2,3,4,5,6,7,8,9,10))
                     # save the IMU data
-                    all_imu_myo_fork_data.append(imu_myo_fork_data)
+                    self.__all_imu_myo_fork_data.append(imu_myo_fork_data)
                 elif "spoon" in file:
                     # use regex to get the user id of spoon IMU data
                     # repeat the same steps as above for the spoon data
                     result = re.search('MyoData/(.*)/spoon', file)
-                    imu_myo_spoon_data_userid.append(result.group(1))
+                    self.__imu_myo_spoon_data_userid.append(result.group(1))
                     imu_myo_spoon_data=np.genfromtxt(file, dtype=float, delimiter=',',usecols=(1,2,3,4,5,6,7,8,9,10))
-                    all_imu_myo_spoon_data.append(imu_myo_spoon_data)
+                    self.__all_imu_myo_spoon_data.append(imu_myo_spoon_data)
                 else:
                     print("unknown file.continue")
 
@@ -98,12 +96,12 @@ class ActivityRecognition:
         # by default this flag should be disabled
         # only print relevant data
         print("IMU MYO SPOON DATA USERS")
-        print(imu_myo_spoon_data_userid)
+        print(self.__imu_myo_spoon_data_userid)
         print("IMU MYO FORK DATA USERS")
-        print(imu_myo_spoon_data_userid)
+        print(self.__imu_myo_spoon_data_userid)
 
         
-        #temp1 = all_imu_myo_spoon_data[0]
+        #temp1 = self.__all_imu_myo_spoon_data[0]
         #plt.figure(1)
         #plt.plot(temp1[:,0])
         #plt.plot(temp1[:,1])
@@ -164,15 +162,15 @@ class ActivityRecognition:
                 userid = 'user09'
             #print(userid)
             #  get the index for this user id
-            for k in range (np.size(imu_myo_fork_data_userid)):
-                if (userid == imu_myo_fork_data_userid[k]):
+            for k in range (np.size(self.__imu_myo_fork_data_userid)):
+                if (userid == self.__imu_myo_fork_data_userid[k]):
                     user_dir_path_fork = out_dir + '/' + userid + '/' + 'fork/'
                     os.makedirs(user_dir_path_fork)
                     uid = k
                     #print("FOUND, ",k)
                     break
             
-            myo_fork_data = all_imu_myo_fork_data[k]
+            myo_fork_data = self.__all_imu_myo_fork_data[k]
 
             # create a temp numpy array of zeros
             eating_action_range = np.zeros([len(fork_data),2], dtype=int)
@@ -209,7 +207,7 @@ class ActivityRecognition:
             extracted_imu_data = []
             non_eating_action_row_indexs = np.where(non_eating_action_row_indexs == 0)[0]
             extracted_imu_data = myo_fork_data[non_eating_action_row_indexs]
-            imu_fork_non_eating.append(extracted_imu_data)
+            self.__imu_fork_non_eating.append(extracted_imu_data)
             
             # save to file the non eating with fork IMU data
             np.savetxt(user_dir_path_fork + non_eating_file_prefix + '.csv',extracted_imu_data, delimiter=',')
@@ -219,7 +217,7 @@ class ActivityRecognition:
 
             # Save the IMU data for fork eating            
             np.savetxt(user_dir_path_fork + file_prefix + '.csv',temp_fork_eating, delimiter=',')
-            imu_fork_eating.append(temp_fork_eating)
+            self.__imu_fork_eating.append(temp_fork_eating)
             temp_fork_eating = []
 
         # @todo: merge to one function
@@ -238,15 +236,15 @@ class ActivityRecognition:
             userid = ground_truth_spoon_data_userid[i]
             #print(userid)
             #  get the index for this user id
-            for k in range (np.size(imu_myo_spoon_data_userid)):
-                if (userid == imu_myo_spoon_data_userid[k]):
+            for k in range (np.size(self.__imu_myo_spoon_data_userid)):
+                if (userid == self.__imu_myo_spoon_data_userid[k]):
                     user_dir_path_spoon = out_dir + '/' + userid + '/' + 'spoon/'
                     os.makedirs(user_dir_path_spoon)
                     uid = k
                     #print("FOUND, ",k)
                     break
             
-            myo_spoon_data = all_imu_myo_spoon_data[k]
+            myo_spoon_data = self.__all_imu_myo_spoon_data[k]
 
             # create a temp numpy array of zeros
             eating_action_range = np.zeros([len(spoon_data),2], dtype=int)
@@ -281,7 +279,7 @@ class ActivityRecognition:
             extracted_imu_data = []
             non_eating_action_row_indexs = np.where(non_eating_action_row_indexs == 0)[0]
             extracted_imu_data = myo_spoon_data[non_eating_action_row_indexs]
-            imu_spoon_non_eating.append(extracted_imu_data)
+            self.__imu_spoon_non_eating.append(extracted_imu_data)
             
             # save to file the non eating with fork IMU data
             np.savetxt(user_dir_path_spoon + non_eating_file_prefix + '.csv',extracted_imu_data, delimiter=',')
@@ -292,7 +290,7 @@ class ActivityRecognition:
 
             # Save the IMU data for fork eating            
             np.savetxt(user_dir_path_spoon + file_prefix + '.csv',temp_spoon_eating, delimiter=',')
-            imu_spoon_eating.append(temp_spoon_eating)
+            self.__imu_spoon_eating.append(temp_spoon_eating)
             temp_spoon_eating = []
         
         print("###########################################################################")
@@ -300,29 +298,141 @@ class ActivityRecognition:
         print("###########################################################################")
 
 
-    def load_data(self):
+    def feature_extraction(self):
         """
-        Load the input data (e.g. the samples to be clusters)
+        Feature extraction step
         Parameters
         ----------
         None
 
         Return
         ------
-        None     
+        None
         """
+        self.acceleration_mean()
+        self.acceleration_variance()
+        self.acceleration_energy()
+        self.energy_spectral_density()
 
 
+    def acceleration_mean(self):
+        """
+        Calculate the mean acceleration for axis x,y,z
+        Parameters
+        ----------
+        None
 
+        Return
+        ------
+        mean acceleration 
+        """
+        print("\n###########################################################################")
+        print("Calculate acceleration mean")
+        print("###########################################################################")
+        # iterate the eating and non eating data for all the users
+        # mean_x, mean_y, mean_z
+        # preprocessing method is a prerequisit for this step
+        #self.__imu_fork_eating = []
+        #self.__imu_spoon_eating = [] 
+        #self.__imu_fork_non_eating = []
+        #self.__imu_spoon_non_eating = []
+        
+        # x,y,z
+        acceleration_mean_eating = np.zeros([len(self.__imu_fork_eating),3],dtype=float)
+        acceleration_mean_non_eating = np.zeros([len(self.__imu_fork_non_eating),3],dtype=float)
+        
+        #acceleration_mean_spoon_eating = np.zeros([len(self.__imu_spoon_eating),3],dtype=float)
+        #acceleration_mean_non_eating_spoon = np.zeros([len(self.__imu_spoon_non_eating),3],dtype=float)
+        
+
+        for i in range(len(self.__imu_fork_eating)):
+            user_fork_eating = self.__imu_fork_eating[i]
+            #user_spoon_eating = self.__imu_spoon_eating[i]
+            bla = np.mean(user_fork_eating,axis = 0)
+
+            acceleration_mean_eating[i][0] = bla[4]
+            acceleration_mean_eating[i][1] = bla[5]
+            acceleration_mean_eating[i][2] = bla[6]
+            #acceleration_mean_eating[i][0] = np.mean(user_fork_eating[:,4])
+            #acceleration_mean_eating[i][1] = np.mean(user_fork_eating[:,5])
+            #acceleration_mean_eating[i][2] = np.mean(user_fork_eating[:,6])
+
+        # take the same number of rows
+        for i in range(len(self.__imu_fork_eating)):
+            user_fork_non_eating = self.__imu_fork_non_eating[i]
+            bla = np.mean(user_fork_non_eating,axis = 0)          
+            acceleration_mean_non_eating[i][0] = bla[4]
+            acceleration_mean_non_eating[i][1] = bla[5]
+            acceleration_mean_non_eating[i][2] = bla[6]
+        
+                
+    
+        plt.figure(1)
+        plt.plot(acceleration_mean_eating[:,0])
+        plt.plot(acceleration_mean_eating[:,1])
+        plt.plot(acceleration_mean_eating[:,2])
+        plt.ylabel('Mean eating - acceleration')
+        #
+        plt.figure(2)
+        plt.plot(acceleration_mean_non_eating[:,0])
+        plt.plot(acceleration_mean_non_eating[:,1])
+        plt.plot(acceleration_mean_non_eating[:,2])
+        plt.ylabel('Mean non eating - acceleration')
+        plt.show()
+    
+    def acceleration_variance(self):
+        """
+        Calculate the variance acceleration for axis x,y,z
+        Parameters
+        ----------
+        None
+
+        Return
+        ------
+        None
+        """
+        print("###########################################################################")
+        print("Calculate acceleration variance")
+        print("###########################################################################")        
+
+    def acceleration_energy(self):
+        """
+        Calculate the acceleration energy for each axis
+        Parameters
+        ----------
+        None
+
+        Return
+        ------
+        None
+        """        
+        print("###########################################################################")
+        print("Calculate acceleration energy")
+        print("###########################################################################")        
+
+    def energy_spectral_density(self):
+        """
+        Feature extraction step
+        Parameters
+        ----------
+        None
+
+        Return
+        ------
+        None
+        """    
+        print("###########################################################################")
+        print("Calculate spectral density")
+        print("###########################################################################")                
     
 
 
 def main():        
 
     activityRecognition = ActivityRecognition()
-
-    activityRecognition.data_setup()
-    
+    activityRecognition.preprocessing()
+    activityRecognition.feature_extraction()
+    #activityRecognition.pca()
 
 if __name__ == "__main__":
     main()
